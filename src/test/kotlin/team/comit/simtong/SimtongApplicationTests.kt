b320fa63d32734d4adb0fc5d4002749eb1516613
KimBeomJin <sdpthf@naver.com>
2022-08-20 23:17:22 +0900

    setting: (#1) init project

---
+++ src/test/kotlin/team/comit/simtong/SimtongApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.comit.simtong
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimtongApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
