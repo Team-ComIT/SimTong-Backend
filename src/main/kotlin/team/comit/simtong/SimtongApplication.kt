9671bbe77df113d02db3d18a0e8a76a7f7716164
KimBeomJin <sdpthf@naver.com>
2022-08-20 23:17:22 +0900

    setting: init project

---
+++ src/main/kotlin/team/comit/simtong/SimtongApplication.kt
@@ -0,0 +1,11 @@
+package team.comit.simtong
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimtongApplication
+
+fun main(args: Array<String>) {
+    runApplication<SimtongApplication>(*args)
+}
