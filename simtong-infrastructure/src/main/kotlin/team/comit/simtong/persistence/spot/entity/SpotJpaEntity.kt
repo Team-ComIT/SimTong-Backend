1ad987d371215e11d317af6fabcc650a354b649e
KimBeomJin <sdpthf@naver.com>
2022-08-21 18:44:58 +0900

    add: (#3) SpotJpaEntity

---
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/spot/entity/SpotJpaEntity.kt
@@ -0,0 +1,20 @@
+package team.comit.simtong.persistence.spot.entity
+
+import org.hibernate.validator.constraints.Length
+import team.comit.simtong.persistence.BaseUUIDEntity
+import javax.persistence.Entity
+import javax.persistence.Table
+import javax.validation.constraints.NotNull
+
+@Entity
+@Table(name = "tbl_spot")
+class SpotJpaEntity(
+
+    @field:NotNull
+    @field:Length(max = 20)
+    val name: String,
+
+    @field:NotNull
+    @field:Length(max = 20)
+    val location: String
+) : BaseUUIDEntity()
