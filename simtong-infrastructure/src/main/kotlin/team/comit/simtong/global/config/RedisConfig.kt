cc953a91a5da68141a076f116d5961a2f7d53ca3
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-22 11:01:20 +0900

    chore: (#7) javadoc 주석 추가

--- simtong-infrastructure/src/main/kotlin/team/comit/simtong/global/config/RedisConfig.kt
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/global/config/RedisConfig.kt
@@ -2,5 +2,12 @@ package team.comit.simtong.global.config
 
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
+/**
+  *
+  * Redis를 설정하는 RedisConfig
+  *
+  * @author JoKyungHyeon
+  * @date 2022/08/22
+ **/
 @EnableRedisRepositories
 class RedisConfig
