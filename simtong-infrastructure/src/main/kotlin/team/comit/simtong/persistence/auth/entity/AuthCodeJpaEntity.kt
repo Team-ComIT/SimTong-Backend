77a1ba12114c276c681e9821e46c7ab0e0dd51b8
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-22 10:12:12 +0900

    add: (#7) AuthCodeJpaEntity

---
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/auth/entity/AuthCodeJpaEntity.kt
@@ -0,0 +1,39 @@
+package team.comit.simtong.persistence.auth.entity
+
+import org.hibernate.annotations.ColumnDefault
+import org.hibernate.validator.constraints.Length
+import org.springframework.data.redis.core.RedisHash
+import java.util.Date
+import javax.persistence.Id
+import javax.persistence.Table
+import javax.validation.constraints.NotNull
+
+/**
+  *
+  * 이메일 인증시 사용자의 이메일인지 확인하는 AuthCodeJpaEntity
+  *
+  * @author JoKyungHyeon
+  * @date 2022/08/22
+ **/
+@RedisHash
+@Table(name = "tbl_auth_code")
+class AuthCodeJpaEntity(
+    @Id
+    val key: String,
+
+    @field:NotNull
+    @field:Length(max = 6)
+    val code: String,
+
+    @field:NotNull
+    val codeExpirationTime: Date,
+
+    @field:NotNull
+    val expirationTime: Int
+) {
+    @field:NotNull
+    @ColumnDefault("1")
+    val attemptCount: Int = 1
+
+    // TODO 재 요청 copy method
+}
