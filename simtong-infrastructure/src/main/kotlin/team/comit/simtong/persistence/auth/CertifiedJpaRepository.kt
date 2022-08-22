de00eadf1302fbad55184104684dc886145ecbcc
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-22 10:11:56 +0900

    add: (#7) CertifiedJpaRepository

---
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/auth/CertifiedJpaRepository.kt
@@ -0,0 +1,16 @@
+package team.comit.simtong.persistence.auth
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+/**
+  *
+  * Spring Repository의 기능을 이용하는 CertifiedJpaRepository
+  *
+  * @author JoKyungHyeon
+  * @date 2022/08/22
+  *
+ **/
+@Repository
+interface CertifiedJpaRepository : CrudRepository<CertifiedJpaRepository, String> {
+}
