7351bee92e7d12fac3ebac356fef5f6f5e9f7ff8
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-29 08:34:34 +0900

    add: (#17) AuthPolicy Repository

---
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/auth/AuthPolicyJpaRepository.kt
@@ -0,0 +1,17 @@
+package team.comit.simtong.persistence.auth
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import team.comit.simtong.persistence.auth.entity.AuthPolicyJpaEntity
+
+/**
+  *
+  * Spring Repository의 기능을 이용하는 AuthPolicyJpaRepository
+  *
+  * @author JoKyungHyeon
+  * @date 2022/08/29
+  * @version 1.0.0
+ **/
+@Repository
+interface AuthPolicyJpaRepository: CrudRepository<AuthPolicyJpaEntity, String> {
+}
