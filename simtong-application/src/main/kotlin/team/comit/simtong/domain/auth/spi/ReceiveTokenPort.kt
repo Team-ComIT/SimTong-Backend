e21081ad5f0dff8a8993a635ec118615d333d3e1
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-01 20:06:42 +0900

    add: (#18) receiveTokenPort

---
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/auth/spi/ReceiveTokenPort.kt
@@ -0,0 +1,18 @@
+package team.comit.simtong.domain.auth.spi
+
+import team.comit.simtong.domain.user.model.Authority
+
+/**
+  *
+  * Jwt 토큰을 반환하는 ReceiveTokenPort
+  *
+  * @author JoKyungHyeon
+  * @date 2022/09/01
+  * @version 1.0.0
+ **/
+interface ReceiveTokenPort {
+
+    fun generateAccessToken(email: String, authority: Authority): String
+
+    fun generateRefreshToken(email: String, authority: Authority): String
+}
