2ff0170e62f0cf75029cd0e2cb1a06c5bfdc6e14
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-01 20:07:21 +0900

    add: (#18) const value

---
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/global/security/token/JwtComponent.kt
@@ -0,0 +1,16 @@
+package team.comit.simtong.global.security.token
+
+/**
+  *
+  * Jwt의 상수를 관리하는 JwtComponent
+  *
+  * @author JoKyungHyeon
+  * @date 2022/09/01
+  * @version 1.0.0
+ **/
+object JwtComponent {
+
+    const val JWT_AUTHORITY = "authority"
+    const val JWT_ACCESS = "access"
+    const val JWT_REFRESH = "refresh"
+}
