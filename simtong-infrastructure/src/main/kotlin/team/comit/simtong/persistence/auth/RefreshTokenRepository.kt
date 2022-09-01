81aff752ac5db33dd84b3363c0d743e2523d85c3
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-01 20:08:25 +0900

    add: (#18) RefreshTokenEntity

---
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/auth/RefreshTokenRepository.kt
@@ -0,0 +1,17 @@
+package team.comit.simtong.persistence.auth
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import team.comit.simtong.persistence.auth.entity.RefreshTokenEntity
+
+/**
+  *
+  * Spring Repository의 기능을 이용하는 RefreshTokenRepository
+  *
+  * @author JoKyungHyeon
+  * @date 2022/09/01
+  * @version 1.0.0
+ **/
+@Repository
+interface RefreshTokenRepository : CrudRepository<RefreshTokenEntity, String> {
+}
---
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/auth/entity/RefreshTokenEntity.kt
@@ -0,0 +1,35 @@
+package team.comit.simtong.persistence.auth.entity
+
+import org.hibernate.validator.constraints.Length
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+import org.springframework.data.redis.core.index.Indexed
+import team.comit.simtong.domain.user.model.Authority
+import javax.validation.constraints.NotNull
+
+/**
+  *
+  * Access 토큰을 재발급하기 위한 RefreshTokenEntity
+  *
+  * @author JoKyungHyeon
+  * @date 2022/09/01
+  * @version 1.0.0
+ **/
+@RedisHash("tbl_refresh_token")
+class RefreshTokenEntity(
+    @Id
+    @Indexed
+    val token: String,
+
+    @field:NotNull
+    val email: String,
+
+    @field:NotNull
+    @field:Length(max = 11)
+    val authority: Authority,
+
+    @field:NotNull
+    @TimeToLive
+    val expirationTime: Int
+)
