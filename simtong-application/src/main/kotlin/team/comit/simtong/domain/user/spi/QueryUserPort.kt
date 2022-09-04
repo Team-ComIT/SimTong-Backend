2106d2004cec48c494e68d848d5d887d80b9f414
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-04 17:40:59 +0900

    add: (#25) User Persistence Adapter & Port

---
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/user/spi/QueryUserPort.kt
@@ -0,0 +1,24 @@
+package team.comit.simtong.domain.user.spi
+
+import team.comit.simtong.domain.user.model.User
+import java.util.UUID
+
+/**
+ *
+ * User에 관한 Query를 요청하는 QueryUserPort
+ *
+ * @author JoKyungHyeon
+ * @date 2022/09/04
+ * @version 1.0.0
+ **/
+interface QueryUserPort {
+
+    fun queryUserById(id: UUID): User
+
+    fun queryUserByEmail(email: String): User
+
+    fun queryUserByNickName(nickName: String): User
+
+    fun existsUserByEmail(email: String): Boolean
+
+}
