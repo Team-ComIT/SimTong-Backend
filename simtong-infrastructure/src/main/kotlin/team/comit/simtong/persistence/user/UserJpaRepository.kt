64444b9513ef1385ae798b255b9fdb11c0413032
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-04 17:41:18 +0900

    refactor: (#25) query User

--- simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/user/UserJpaRepository.kt
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/user/UserJpaRepository.kt
@@ -1,5 +1,6 @@
 package team.comit.simtong.persistence.user
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import team.comit.simtong.persistence.user.entity.UserJpaEntity
@@ -10,12 +11,20 @@ import java.util.*
  * Spring Repository의 기능을 이용하는 UserJpaRepository
  *
  * @author kimbeomjin
+ * @author JoKyungHyeon
  * @date 2022/08/21
- * @version 1.0.0
+ * @version 1.1.0
  **/
 @Repository
 interface UserJpaRepository : CrudRepository<UserJpaEntity, UUID> {
 
     fun queryUserJpaEntityById(id: UUID): UserJpaEntity?
+
+    fun queryUserJpaEntityByEmail(email: String): UserJpaEntity?
+
+    fun queryUserJpaEntityByNickname(nickName: String): UserJpaEntity?
+
+    fun existsUserJpaEntitiesByEmail(email: String): Boolean
+
 
 }
