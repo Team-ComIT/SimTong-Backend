a657083158e791c4bbc8f31dd24c15476d3afe10
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-04 19:19:50 +0900

    style: (#25) 누락된 주석 추가

--- simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/spi/SecurityPort.kt
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/spi/SecurityPort.kt
@@ -1,5 +1,13 @@
 package team.comit.simtong.domain.user.spi
 
+/**
+ *
+ * 비밀번호 암호화를 담당하는 SecurityPort
+ *
+ * @author Chokyunghyeon
+ * @date 2022/09/04
+ * @version 1.0.0
+ **/
 interface SecurityPort {
 
     fun compare(target: String, encryptedPassword: String): Boolean
