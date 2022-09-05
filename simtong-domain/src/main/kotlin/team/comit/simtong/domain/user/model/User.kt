307209b19546b16ed711076fc8b1d3f4fc4ecdab
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-05 21:49:28 +0900

    refactor: (#25) policy 추가

--- simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/model/User.kt
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/model/User.kt
@@ -1,8 +1,6 @@
 package team.comit.simtong.domain.user.model
 
-import team.comit.simtong.domain.user.spi.SecurityPort
 import team.comit.simtong.global.annotation.Aggregate
-import java.time.LocalDateTime
 import java.util.UUID
 
 /**
@@ -35,23 +33,7 @@ data class User(
 ) {
 
     companion object {
-        private const val defaultImage = "" // TODO 기본 프로필 이미지 설정
-
-        fun signUp(name: String, email: String, password: String,
-                   nickname: String, employeeNumber: Int, profileImagePath: String?): User {
-
-            return User(
-                id = null,
-                name = name,
-                email = email,
-                password = SecurityPort::encode.run { password },
-                nickname = nickname,
-                employeeNumber = employeeNumber,
-                authority = Authority.ROLE_COMMON,
-                adminCode = null,
-                profileImagePath = profileImagePath ?: defaultImage
-            )
-        }
+        const val defaultImage = "" // TODO 기본 프로필 이미지 설정
     }
 
 }
