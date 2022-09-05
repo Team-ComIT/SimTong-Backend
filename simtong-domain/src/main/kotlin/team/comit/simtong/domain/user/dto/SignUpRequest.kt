a64deab49b1d6063f662edba6b32126073464869
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-05 21:46:35 +0900

    refactor: (#25) SignUpRequest 계층 변경

--- simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/dto/SignUpRequest.kt
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/dto/SignUpRequest.kt
@@ -1,21 +1,21 @@
-package team.comit.simtong.domain.user.usecase.dto
+package team.comit.simtong.domain.user.dto
 
 /**
  *
- * 회원 가입 정보를 전달하는 DomainSignUpRequest
+ * 회원 가입 정보를 전달하는 SignUpRequest
  *
  * @author Chokyunghyeon
  * @date 2022/09/04
  * @version 1.0.0
  **/
-data class DomainSignUpRequest(
+data class SignUpRequest(
     val name: String,
 
     val email: String,
 
     val password: String,
 
-    val nickname: String,
+    val nickname: String?,
 
     val employeeNumber: Int,
 
