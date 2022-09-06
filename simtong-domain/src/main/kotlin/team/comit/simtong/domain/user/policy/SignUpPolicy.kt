0af16331b1c78e0e05bc523ae512ce6d01e3598c
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-06 18:57:35 +0900

    refactor: (#25) 임시 코드로 변경

--- simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/policy/SignUpPolicy.kt
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/policy/SignUpPolicy.kt
@@ -1,10 +1,11 @@
 package team.comit.simtong.domain.user.policy
 
+import org.springframework.stereotype.Component
 import team.comit.simtong.domain.user.dto.DomainSignUpRequest
 import team.comit.simtong.domain.user.model.Authority
 import team.comit.simtong.domain.user.model.User
-import team.comit.simtong.domain.user.spi.CheckEmailPort
-import team.comit.simtong.domain.user.spi.NickNamePort
+//import team.comit.simtong.domain.user.spi.CheckEmailPort
+//import team.comit.simtong.domain.user.spi.NickNamePort
 import team.comit.simtong.domain.user.spi.SecurityPort
 
 /**
@@ -15,16 +16,17 @@ import team.comit.simtong.domain.user.spi.SecurityPort
  * @date 2022/09/05
  * @version 1.0.0
  **/
+@Component
 class SignUpPolicy(
-    private val checkEmailPort: CheckEmailPort,
-    private val securityPort: SecurityPort,
-    private val randomNamePort: NickNamePort
+//    private val checkEmailPort: CheckEmailPort,
+//    private val randomNamePort: NickNamePort,
+    private val securityPort: SecurityPort
 ) {
 
     fun implement(request: DomainSignUpRequest): User {
 
-        checkEmailPort.checkCertifiedEmail(request.email)
-        checkEmailPort.checkUsedEmail(request.email)
+//        checkEmailPort.checkCertifiedEmail(request.email)
+//        checkEmailPort.checkUsedEmail(request.email)
 
         // TODO name & employeeNumber 확인 로직
 
@@ -33,7 +35,8 @@ class SignUpPolicy(
             name = request.name,
             email = request.email,
             password = securityPort.encode(request.password),
-            nickname = request.nickname ?: randomNamePort.random(),
+//            nickname = request.nickname ?: randomNamePort.random(),
+            nickname = request.nickname ?: "TODO",
             employeeNumber = request.employeeNumber,
             authority = Authority.ROLE_COMMON,
             adminCode = null,
