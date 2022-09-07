20143f2dc29da671e262abd96bba757479671215
KimBeomJin <sdpthf@naver.com>
2022-09-07 22:11:55 +0900

    chore: (#38) spring-tx dependency 추가

--- simtong-application/build.gradle.kts
+++ simtong-application/build.gradle.kts
@@ -1,3 +1,17 @@
+plugins {
+    id("org.jetbrains.kotlin.plugin.allopen") version PluginVersions.ALLOPEN_VERSION
+}
+
 dependencies {
+
+    // spring transaction
+    implementation(Dependencies.SPRING_TRANSACTION)
+
+    // api project
     api(project(":simtong-domain"))
+}
+
+allOpen {
+    annotation("team.comit.simtong.global.annotation.UseCase")
+    annotation("team.comit.simtong.global.annotation.ReadOnlyUseCase")
 }
