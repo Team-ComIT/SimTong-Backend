a9abc907839b0b29428604d52ccc26786534700d
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-09 01:46:59 +0900

    add: (#31) Aws SES Test Case

---
+++ simtong-infrastructure/src/test/kotlin/team/comit/simtong/thirdparty/email/AwsSESAdapterTests.kt
@@ -0,0 +1,10 @@
+package team.comit.simtong.thirdparty.email
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+class AwsSESAdapterTests {
+
+}
