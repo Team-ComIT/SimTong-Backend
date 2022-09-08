1650bd5d724dd426d8654e689ee915d4fec59ec4
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-08 13:41:13 +0900

    refactor: (#31) 책임 분리

--- simtong-infrastructure/src/main/kotlin/team/comit/simtong/thirdparty/storage/AwsS3Adapter.kt
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/thirdparty/storage/AwsS3Adapter.kt
@@ -7,7 +7,7 @@ import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.model.PutObjectRequest
 import org.springframework.stereotype.Component
 import team.comit.simtong.domain.file.exception.FileExtensionInvalidException
-import team.comit.simtong.domain.user.spi.UploadFilePort
+import team.comit.simtong.domain.file.spi.ManageFilePort
 import team.comit.simtong.domain.file.exception.FileIOInterruptedException
 import java.io.File
 import java.io.IOException
@@ -22,31 +22,35 @@ import java.util.*
  * @version 1.0.0
  **/
 @Component
-class FileUploadAdapter(
+class AwsS3Adapter(
     private val awsProperties: AwsS3Properties,
     private val amazonS3Client: AmazonS3Client
-): UploadFilePort {
+): ManageFilePort {
 
-    override fun fileUpload(file: File): String {
+    override fun upload(file: File): String {
         if(file.extension != "jpa" && file.extension != "jpeg" && file.extension != "png") {
             throw FileExtensionInvalidException.EXCEPTION
         }
 
-        val fileName = "${UUID.randomUUID()}/${file.name}"
-        return inputS3(file, fileName)
+        val fileName = "${UUID.randomUUID()}${file.name}"
+        inputS3(file, fileName)
+
+        return getResource(fileName)
     }
 
-    private fun inputS3(file: File, fileName: String): String {
+    private fun inputS3(file: File, fileName: String) {
         try {
             val inputStream = file.inputStream()
             val objectMetadata = ObjectMetadata()
             objectMetadata.contentType = Mimetypes.getInstance().getMimetype(file)
 
             amazonS3Client.putObject(PutObjectRequest(awsProperties.bucket, fileName, inputStream, objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead))
-
-            return amazonS3Client.getResourceUrl(awsProperties.bucket, fileName)
         } catch (e: IOException) {
             throw FileIOInterruptedException.EXCEPTION
         }
+    }
+
+    private fun getResource(fileName: String): String {
+        return amazonS3Client.getResourceUrl(awsProperties.bucket, fileName)
     }
 }
