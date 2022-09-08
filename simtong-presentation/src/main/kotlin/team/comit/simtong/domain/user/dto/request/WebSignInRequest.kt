64ba6c94bc013448f055e499596620d86bfd213f
KimBeomJin <sdpthf@naver.com>
2022-09-08 11:40:40 +0900

    add: (#32) WebSignInRequest

---
+++ simtong-presentation/src/main/kotlin/team/comit/simtong/domain/user/dto/request/WebSignInRequest.kt
@@ -0,0 +1,22 @@
+package team.comit.simtong.domain.user.dto.request
+
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+
+/**
+ *
+ * 로그인을 요청하는 WebSignInRequest
+ *
+ * @author kimbeomjin
+ * @date 2022/09/08
+ * @version 1.0.0
+ **/
+data class WebSignInRequest(
+
+    @field:NotNull
+    val employeeNumber: Int,
+
+    @field:NotBlank
+    val password: String
+)
