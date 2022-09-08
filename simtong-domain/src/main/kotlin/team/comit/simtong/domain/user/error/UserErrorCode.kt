1a795d8b943949ea0e2c0d03eb2cd88a347073d8
KimBeomJin <sdpthf@naver.com>
2022-09-08 11:41:33 +0900

    add: (#32) DifferentPasswordException

--- simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/error/UserErrorCode.kt
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/error/UserErrorCode.kt
@@ -7,6 +7,7 @@ import team.comit.simtong.global.error.ErrorProperty
  * User에 대해 발생하는 Error를 관리하는 UserErrorCode
  *
  * @author Chokyunghyeon
+ * @author kimbeomjin
  * @date 2022/09/04
  * @version 1.0.0
  **/
@@ -15,9 +16,14 @@ enum class UserErrorCode(
     private val message: String
 ): ErrorProperty {
 
-    ALREADY_USED_EMAIL(409, "이미 사용된 이메일"),
+    // 401
+    DIFFERENT_PASSWORD(401, "비밀번호가 일치하지 않음"),
 
-    USER_NOT_FOUND(401, "유저를 찾을 수 없음");
+    // 404
+    USER_NOT_FOUND(404, "유저를 찾을 수 없음"),
+
+    // 409
+    ALREADY_USED_EMAIL(409, "이미 사용된 이메일");
 
     override fun status(): Int = status
 
