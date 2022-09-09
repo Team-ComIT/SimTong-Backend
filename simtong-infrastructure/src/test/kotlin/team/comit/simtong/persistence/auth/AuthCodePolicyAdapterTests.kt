0727b5f2a3534c2e596e09ebb2ba25eb65a35399
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-10 01:32:43 +0900

    add: (#34) AuthCodePolicyAdapterTests

---
+++ simtong-infrastructure/src/test/kotlin/team/comit/simtong/persistence/auth/AuthCodePolicyAdapterTests.kt
@@ -0,0 +1,58 @@
+package team.comit.simtong.persistence.auth
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import team.comit.simtong.domain.auth.exception.UncertifiedEmailException
+import team.comit.simtong.persistence.auth.entity.AuthCodePolicyEntity
+import team.comit.simtong.persistence.auth.repository.AuthCodePolicyRepository
+
+@ExtendWith(SpringExtension::class)
+class AuthCodePolicyAdapterTests {
+
+    @MockBean
+    private lateinit var authCodePolicyRepository: AuthCodePolicyRepository
+
+    private lateinit var authCodePolicyAdapter: AuthCodePolicyAdapter
+
+    private val email: String = "test@test.com"
+
+    private val authCodePolicyEntityStub: AuthCodePolicyEntity by lazy {
+        AuthCodePolicyEntity(
+            key = email,
+            expirationTime = 123456789
+        )
+    }
+
+    @BeforeEach
+    fun setUp() {
+        authCodePolicyAdapter = AuthCodePolicyAdapter(authCodePolicyRepository)
+    }
+
+    @Test
+    fun `정책 인증 확인`() {
+        // given
+        given(authCodePolicyRepository.queryAuthCodePolicyEntityByKey(email))
+            .willReturn(authCodePolicyEntityStub)
+
+        // when & then
+        assertFalse(authCodePolicyAdapter.checkCertifiedEmail(email))
+    }
+
+    @Test
+    fun `정책 인증 미확인`() {
+        // given
+        given(authCodePolicyRepository.queryAuthCodePolicyEntityByKey(email))
+            .willReturn(null)
+
+        assertThrows<UncertifiedEmailException> {
+            authCodePolicyAdapter.checkCertifiedEmail(email)
+        }
+    }
+}
