10a627a4f3ec6149f48fd9b3d98764c5808d7247
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-09 19:06:34 +0900

    add: (#34) AuthCodePolicyAdapter

---
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/auth/AuthCodePolicyAdapter.kt
@@ -0,0 +1,26 @@
+package team.comit.simtong.persistence.auth
+
+import org.springframework.stereotype.Component
+import team.comit.simtong.domain.auth.exception.UncertifiedEmailException
+import team.comit.simtong.domain.user.spi.CheckAuthCodePolicyPort
+import team.comit.simtong.persistence.auth.repository.AuthCodePolicyRepository
+
+/**
+ *
+ * AuthCodePolicy를 관리하는 AuthCodePolicyAdapter
+ *
+ * @author Chokyunghyeon
+ * @date 2022/09/09
+ * @version 1.0.0
+ **/
+@Component
+class AuthCodePolicyAdapter(
+    private val authCodePolicyRepository: AuthCodePolicyRepository
+): CheckAuthCodePolicyPort {
+
+    override fun checkCertifiedEmail(email: String): Boolean {
+        val authCodePolicy = authCodePolicyRepository.queryAuthCodePolicyEntityByKey(email) ?: throw UncertifiedEmailException.EXCEPTION
+
+        return authCodePolicy.isVerified
+    }
+}
