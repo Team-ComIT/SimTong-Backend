5d4422eabf8a59b5e2009fb08d479c8241768caa
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-11 17:17:26 +0900

    refactor: (#54) TestCase 완료

--- simtong-application/src/test/kotlin/team/comit/simtong/domain/user/usecase/FindEmployeeNumberUseCaseTests.kt
+++ simtong-application/src/test/kotlin/team/comit/simtong/domain/user/usecase/FindEmployeeNumberUseCaseTests.kt
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.BDDMockito.given
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import team.comit.simtong.domain.user.model.Authority
 import team.comit.simtong.domain.user.model.User
 import team.comit.simtong.domain.user.spi.QueryUserPort
+import team.comit.simtong.domain.user.usecase.dto.FindEmployeeNumberRequest
 
 @ExtendWith(SpringExtension::class)
 class FindEmployeeNumberUseCaseTests {
@@ -19,9 +21,31 @@ class FindEmployeeNumberUseCaseTests {
 
     private lateinit var findEmployeeNumberUseCase: FindEmployeeNumberUseCase
 
+    private val name: String = "test name"
+
+    private val email: String = "test email"
+
+    private val spot: String = "test spot"
+
+    private val employeeNumber: Int = 1234567891
+
     private val userStub: User by lazy {
         User(
-            
+            nickname = "test nickname",
+            name = "test name",
+            email = "test email",
+            employeeNumber = employeeNumber,
+            password = "test password",
+            authority = Authority.ROLE_COMMON,
+            profileImagePath = "test path"
+        )
+    }
+
+    private val requestStub: FindEmployeeNumberRequest by lazy {
+        FindEmployeeNumberRequest(
+            name = name,
+            spot = spot,
+            email = email
         )
     }
 
@@ -31,9 +55,13 @@ class FindEmployeeNumberUseCaseTests {
     }
 
     @Test
-    fun `사원 번호 찾기 성공`() {
+    fun `사원 번호 찾기`() {
         // given
-        given(queryUserPort.queryUserByNameAndSpotAndEmail())
+        given(queryUserPort.queryUserByNameAndSpotAndEmail(name, spot, email))
+            .willReturn(userStub)
+
+        // when & then
+        assertEquals(findEmployeeNumberUseCase.execute(requestStub), employeeNumber)
     }
 
 }
