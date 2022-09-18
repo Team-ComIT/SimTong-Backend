1a0cfd429a8860708243eb7551d2a03374f12ee4
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-18 14:35:20 +0900

    add: (#70) Team Persistence 설계

---
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/team/mapper/TeamMapper.kt
@@ -0,0 +1,31 @@
+package team.comit.simtong.persistence.team.mapper
+
+import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.springframework.beans.factory.annotation.Autowired
+import team.comit.simtong.domain.team.model.Team
+import team.comit.simtong.persistence.GenericMapper
+import team.comit.simtong.persistence.spot.SpotJpaRepository
+import team.comit.simtong.persistence.team.entity.TeamJpaEntity
+
+/**
+ *
+ * TeamEntity와 DomainTeam을 변환하는 TeamMapper
+ *
+ * @author Chokyunghyeon
+ * @date 2022/09/18
+ * @version 1.0.0
+ **/
+@Mapper
+abstract class TeamMapper : GenericMapper<TeamJpaEntity, Team>{
+
+    @Autowired
+    protected lateinit var spotJpaRepository: SpotJpaRepository
+
+    @Mapping(target = "spot", expression = "java(spotJpaRepository.querySpotJpaEntityById(model.getSpotId()))")
+    abstract override fun toEntity(model: Team): TeamJpaEntity
+
+    @Mapping(target = "spotId", expression = "java(entity.getId())")
+    abstract override fun toDomain(entity: TeamJpaEntity?): Team?
+
+}
