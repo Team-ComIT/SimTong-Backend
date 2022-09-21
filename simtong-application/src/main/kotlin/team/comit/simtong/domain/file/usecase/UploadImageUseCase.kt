37e9ccb802be1a78416715984ab325eb8ef206c5
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-21 22:17:26 +0900

    add: (#79) 이미지 업로드 UseCase

---
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/file/usecase/UploadImageUseCase.kt
@@ -0,0 +1,28 @@
+package team.comit.simtong.domain.file.usecase
+
+import team.comit.simtong.domain.file.spi.ManageFilePort
+import team.comit.simtong.global.annotation.UseCase
+import java.io.File
+
+/**
+ *
+ * 이미지 업로드 기능을 담당하는 UploadImageUseCase
+ *
+ * @author Chokyunghyeon
+ * @date 2022/09/20
+ * @version 1.0.0
+ **/
+@UseCase
+class UploadImageUseCase(
+    private val manageFilePort: ManageFilePort
+) {
+
+    fun execute(file: File): String {
+        return manageFilePort.upload(file);
+    }
+
+    fun execute(files: List<File>): List<String> {
+        return manageFilePort.upload(files)
+    }
+    
+}
