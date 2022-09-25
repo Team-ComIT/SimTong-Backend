3cafa35be722649a6214f42cd774e374d372e029
KimBeomJin <sdpthf@naver.com>
2022-09-25 22:43:18 +0900

    add: (#80) QueryMenuPort

---
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/menu/spi/QueryMenuPort.kt
@@ -0,0 +1,18 @@
+package team.comit.simtong.domain.menu.spi
+
+import team.comit.simtong.domain.menu.model.Menu
+import java.time.LocalDate
+
+/**
+ *
+ * Menu에 관한 Query룰 요청하는 QueryMenuPort
+ *
+ * @author kimbeomjin
+ * @date 2022/09/21
+ * @version 1.0.0
+ **/
+interface QueryMenuPort {
+
+    fun queryMenuByMonth(startDate: LocalDate, endDate: LocalDate): List<Menu?>
+
+}
