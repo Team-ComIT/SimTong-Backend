db2fe75bac8b87a636b3b80261924de7975c37a5
KimBeomJin <sdpthf@naver.com>
2022-09-26 09:12:43 +0900

    chore: (#80) MenuResponse Javadoc 추가

--- simtong-application/src/main/kotlin/team/comit/simtong/domain/menu/dto/MenuResponse.kt
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/menu/dto/MenuResponse.kt
@@ -2,6 +2,14 @@ package team.comit.simtong.domain.menu.dto
 
 import java.time.LocalDate
 
+/**
+ *
+ * 메뉴를 반환하는 MenuResponse
+ *
+ * @author kimbeomjin
+ * @date 2022/09/26
+ * @version 1.0.0
+ **/
 data class MenuResponse(
     val menu: List<MenuElement>
 ) {
