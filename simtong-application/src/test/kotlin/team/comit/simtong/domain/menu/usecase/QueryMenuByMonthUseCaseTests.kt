860798925d910404eb4c5f5d8828dbc178a8adf8
KimBeomJin <sdpthf@naver.com>
2022-09-26 23:07:11 +0900

    chore: (#80) 메뉴 조회 테스트 코드 수정

--- simtong-application/src/test/kotlin/team/comit/simtong/domain/menu/usecase/QueryMenuByMonthUseCaseTests.kt
+++ simtong-application/src/test/kotlin/team/comit/simtong/domain/menu/usecase/QueryMenuByMonthUseCaseTests.kt
@@ -3,12 +3,18 @@ package team.comit.simtong.domain.menu.usecase
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.BDDMockito.given
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import team.comit.simtong.domain.menu.model.Menu
+import team.comit.simtong.domain.menu.spi.MenuQueryUserPort
+import team.comit.simtong.domain.menu.spi.MenuSecurityPort
 import team.comit.simtong.domain.menu.spi.QueryMenuPort
+import team.comit.simtong.domain.user.exception.UserNotFoundException
+import team.comit.simtong.domain.user.model.Authority
+import team.comit.simtong.domain.user.model.User
 import java.time.LocalDate
 import java.util.*
 
@@ -18,7 +24,32 @@ class QueryMenuByMonthUseCaseTests {
     @MockBean
     private lateinit var queryMenuPort: QueryMenuPort
 
+    @MockBean
+    private lateinit var queryUserPort: MenuQueryUserPort
+
+    @MockBean
+    private lateinit var menuSecurityPort: MenuSecurityPort
+
     private lateinit var queryMenuByMonthUseCase: QueryMenuByMonthUseCase
+
+    private val currentUserId = UUID.randomUUID()
+    private val spotId = UUID.randomUUID()
+    private val now = LocalDate.now()
+
+    private val userStub: User by lazy {
+        User(
+            id = currentUserId,
+            name = "test name",
+            nickname = "test nickname",
+            email = "test email",
+            password = "test encode password",
+            employeeNumber = 1234567891,
+            authority = Authority.ROLE_COMMON,
+            spotId = spotId,
+            teamId = UUID.randomUUID(),
+            profileImagePath = "test profileImagePath"
+        )
+    }
 
     private val menuStub: Menu by lazy {
         Menu(
@@ -38,25 +69,43 @@ class QueryMenuByMonthUseCaseTests {
 
     @BeforeEach
     fun setUp() {
-        queryMenuByMonthUseCase = QueryMenuByMonthUseCase(queryMenuPort)
+        queryMenuByMonthUseCase = QueryMenuByMonthUseCase(queryMenuPort, queryUserPort, menuSecurityPort)
     }
 
     @Test
     fun `메뉴 조회 성공`() {
         // given
-        val spotId = UUID.randomUUID()
-        val now = LocalDate.now()
+        given(menuSecurityPort.getCurrentUserId())
+            .willReturn(currentUserId)
 
-        given(queryMenuPort.queryMenuByMonth(now.year, now.monthValue, spotId))
+        given(queryUserPort.queryUserById(currentUserId))
+            .willReturn(userStub)
+
+        given(queryMenuPort.queryMenuByMonth(now.year, now.monthValue, userStub.spotId))
             .willReturn(
                 listOf(menuStub, menuStub2)
             )
 
         // when
-        val response = queryMenuByMonthUseCase.execute(now, spotId)
+        val response = queryMenuByMonthUseCase.execute(now)
 
         // then
         assertThat(response).isNotNull
+    }
+
+    @Test
+    fun `유저가 존재하지 않음`() {
+        // given
+        given(menuSecurityPort.getCurrentUserId())
+            .willReturn(currentUserId)
+
+        given(queryUserPort.queryUserById(currentUserId))
+            .willReturn(null)
+
+        // when & then
+        assertThrows<UserNotFoundException> {
+            queryMenuByMonthUseCase.execute(now)
+        }
     }
 
 }
