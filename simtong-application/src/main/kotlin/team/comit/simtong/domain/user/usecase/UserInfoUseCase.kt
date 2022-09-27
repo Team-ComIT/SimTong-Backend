c4367c88f6d36e65d811a095eeeccb4d2feabbe1
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-27 19:42:54 +0900

    add: (#86) 유저 정보 보기 로직

---
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/user/usecase/UserInfoUseCase.kt
@@ -0,0 +1,40 @@
+package team.comit.simtong.domain.user.usecase
+
+import team.comit.simtong.domain.spot.exception.SpotNotFoundException
+import team.comit.simtong.domain.user.dto.UserInfoResponse
+import team.comit.simtong.domain.user.exception.UserNotFoundException
+import team.comit.simtong.domain.user.spi.QueryUserPort
+import team.comit.simtong.domain.user.spi.UserQuerySpotPort
+import team.comit.simtong.domain.user.spi.UserSecurityPort
+import team.comit.simtong.global.annotation.ReadOnlyUseCase
+
+/**
+ *
+ * 사용자 정보 보기를 담당하는 GetInfoUseCase
+ *
+ * @author Chokyunghyeon
+ * @date 2022/09/27
+ * @version 1.0.0
+ **/
+@ReadOnlyUseCase
+class UserInfoUseCase(
+    private val queryUserPort: QueryUserPort,
+    private val userSecurityPort: UserSecurityPort,
+    private val userQuerySpotPort: UserQuerySpotPort
+) {
+
+    fun execute(): UserInfoResponse {
+        val id = userSecurityPort.getCurrentUserId()
+
+        val user = queryUserPort.queryUserById(id) ?: throw UserNotFoundException.EXCEPTION
+        val spot = userQuerySpotPort.querySpotById(user.spotId) ?: throw SpotNotFoundException.EXCEPTION
+
+        return UserInfoResponse(
+            name = user.name,
+            email = user.email,
+            nickName = user.nickname,
+            spot = spot.name,
+            profileImagePath = user.profileImagePath
+        )
+    }
+}
--- simtong-presentation/src/main/kotlin/team/comit/simtong/user/WebUserAdapter.kt
+++ simtong-presentation/src/main/kotlin/team/comit/simtong/user/WebUserAdapter.kt
@@ -1,18 +1,21 @@
 package team.comit.simtong.user
 
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import team.comit.simtong.domain.auth.dto.TokenResponse
-import team.comit.simtong.user.dto.request.WebSignInRequest
-import team.comit.simtong.user.dto.request.WebSignUpRequest
-import team.comit.simtong.domain.user.usecase.SignInUseCase
-import team.comit.simtong.domain.user.usecase.SignUpUseCase
 import team.comit.simtong.domain.user.dto.SignInRequest
 import team.comit.simtong.domain.user.dto.SignUpRequest
+import team.comit.simtong.domain.user.dto.UserInfoResponse
+import team.comit.simtong.domain.user.usecase.UserInfoUseCase
+import team.comit.simtong.domain.user.usecase.SignInUseCase
+import team.comit.simtong.domain.user.usecase.SignUpUseCase
+import team.comit.simtong.user.dto.request.WebSignInRequest
+import team.comit.simtong.user.dto.request.WebSignUpRequest
 import javax.validation.Valid
 
 /**
@@ -28,7 +31,8 @@ import javax.validation.Valid
 @RequestMapping("/users")
 class WebUserAdapter(
     private val signUpUseCase: SignUpUseCase,
-    private val signInUseCase: SignInUseCase
+    private val signInUseCase: SignInUseCase,
+    private val getInfoUseCase: UserInfoUseCase
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -54,6 +58,11 @@ class WebUserAdapter(
                 password = request.password
             )
         )
+    }
+
+    @GetMapping("/information")
+    fun getInfo(): UserInfoResponse {
+        return getInfoUseCase.execute()
     }
 
 }
