4312a84376e8a1a387b536ca9d28bc981f44c5ff
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-09-30 17:11:36 +0900

    revert: (#78) allOpen DomainService

--- simtong-domain/build.gradle.kts
+++ simtong-domain/build.gradle.kts
@@ -1,10 +1,2 @@
-plugins {
-    kotlin("plugin.allopen") version PluginVersions.ALLOPEN_VERSION
-}
-
 dependencies {
-}
-
-allOpen {
-    annotation("team.comit.simtong.global.annotation.DomainService")
 }
