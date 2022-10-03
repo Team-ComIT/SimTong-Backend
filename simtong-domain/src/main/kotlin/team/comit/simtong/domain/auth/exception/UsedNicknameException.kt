d019f6e6d8b3c5f5b833c423083ae868e059604d
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-03 17:08:37 +0900

    docs: (#94) 누락된 javadoc 추가

--- simtong-domain/src/main/kotlin/team/comit/simtong/domain/auth/exception/UsedNicknameException.kt
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/auth/exception/UsedNicknameException.kt
@@ -3,6 +3,14 @@ package team.comit.simtong.domain.auth.exception
 import team.comit.simtong.domain.auth.error.AuthErrorCode
 import team.comit.simtong.global.error.BusinessException
 
+/**
+ *
+ * Already Used Nickname Error를 발생시키는 UsedNicknameException
+ *
+ * @author Chokyunghyeon
+ * @date 2022/10/03
+ * @version 1.0.0
+ **/
 class UsedNicknameException private constructor() : BusinessException(AuthErrorCode.ALREADY_USED_NICKNAME) {
 
     companion object {
--- simtong-presentation/src/main/kotlin/team/comit/simtong/user/dto/request/WebChangeNicknameRequest.kt
+++ simtong-presentation/src/main/kotlin/team/comit/simtong/user/dto/request/WebChangeNicknameRequest.kt
@@ -4,6 +4,14 @@ import org.hibernate.validator.constraints.Length
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Pattern
 
+/**
+ *
+ * 닉네임 변경을 요청하는 WebChangeNicknameRequest
+ *
+ * @author Chokyunghyeon
+ * @date 2022/10/03
+ * @version 1.0.0
+ **/
 data class WebChangeNicknameRequest(
     @field:NotBlank
     @field:Pattern(regexp = """^[가-힣][\s_.가-힣]*""")
