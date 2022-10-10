3cf476cf9e166667e1f11c6e9948fc4e2ce3430e
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-10 14:01:53 +0900

    test: (#99) 프로필 사진 변경 TestCase

---
+++ simtong-application/src/test/kotlin/team/comit/simtong/domain/user/usecase/ChangeProfileImageUseCaseTests.kt
@@ -0,0 +1,96 @@
+package team.comit.simtong.domain.user.usecase
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import team.comit.simtong.domain.user.dto.ChangeProfileImageRequest
+import team.comit.simtong.domain.user.exception.UserNotFoundException
+import team.comit.simtong.domain.user.model.Authority
+import team.comit.simtong.domain.user.model.User
+import team.comit.simtong.domain.user.spi.CommandUserPort
+import team.comit.simtong.domain.user.spi.QueryUserPort
+import team.comit.simtong.domain.user.spi.UserSecurityPort
+import java.util.UUID
+
+@ExtendWith(SpringExtension::class)
+class ChangeProfileImageUseCaseTests {
+
+    @MockBean
+    private lateinit var queryUserPort: QueryUserPort
+
+    @MockBean
+    private lateinit var userSecurityPort: UserSecurityPort
+
+    @MockBean
+    private lateinit var commandUserPort: CommandUserPort
+
+    private lateinit var changeProfileImageUseCase: ChangeProfileImageUseCase
+
+    private val id = UUID.randomUUID()
+
+    private val requestStub: ChangeProfileImageRequest by lazy {
+        ChangeProfileImageRequest(
+            profileImagePath = "test path"
+        )
+    }
+
+    private val userStub: User by lazy {
+        User(
+            id = id,
+            nickname = "test nickname",
+            name = "test name",
+            email = "test email",
+            password = "test password",
+            employeeNumber = 1234567890,
+            authority = Authority.ROLE_COMMON,
+            spotId = id,
+            teamId = id,
+            profileImagePath = "test path"
+        )
+    }
+
+    @BeforeEach
+    fun setUp() {
+        changeProfileImageUseCase = ChangeProfileImageUseCase(
+            queryUserPort,
+            userSecurityPort,
+            commandUserPort
+        )
+    }
+
+    @Test
+    fun `프로필 사진 변경 성공`() {
+        // given
+        given(userSecurityPort.getCurrentUserId())
+            .willReturn(id)
+
+        given(queryUserPort.queryUserById(id))
+            .willReturn(userStub)
+
+        // when & then
+        assertDoesNotThrow {
+            changeProfileImageUseCase.execute(requestStub)
+        }
+    }
+
+    @Test
+    fun `유저를 찾을 수 없음`() {
+        // given
+        given(userSecurityPort.getCurrentUserId())
+            .willReturn(id)
+
+        given(queryUserPort.queryUserById(id))
+            .willReturn(null)
+
+        // when & then
+        assertThrows<UserNotFoundException> {
+            changeProfileImageUseCase.execute(requestStub)
+        }
+    }
+
+}
