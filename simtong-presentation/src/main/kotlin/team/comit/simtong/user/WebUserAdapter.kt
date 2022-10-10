82350ced04be59895c319527d1ed62964867c5fa
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-10 20:21:20 +0900

    fix: (#99) 병합 누락

--- simtong-presentation/src/main/kotlin/team/comit/simtong/user/WebUserAdapter.kt
+++ simtong-presentation/src/main/kotlin/team/comit/simtong/user/WebUserAdapter.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import team.comit.simtong.domain.auth.dto.TokenResponse
 import team.comit.simtong.domain.user.dto.ChangeEmailRequest
 import team.comit.simtong.domain.user.dto.ChangeNicknameRequest
 import team.comit.simtong.domain.user.dto.ChangeProfileImageRequest
