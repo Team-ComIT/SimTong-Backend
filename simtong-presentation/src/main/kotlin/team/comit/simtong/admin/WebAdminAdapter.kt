b656c0c186e861ef1aa347d488b8a147e674b63c
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-12 19:12:03 +0900

    refactor: (#101) 필요없는 HttpStatus 삭제

--- simtong-presentation/src/main/kotlin/team/comit/simtong/admin/WebAdminAdapter.kt
+++ simtong-presentation/src/main/kotlin/team/comit/simtong/admin/WebAdminAdapter.kt
@@ -1,10 +1,8 @@
 package team.comit.simtong.admin
 
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import team.comit.simtong.domain.auth.dto.TokenResponse
 import team.comit.simtong.domain.user.dto.SignInRequest
