4ce892985dedaee75c1a8c67216c8a9e1e90cada
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-12 13:20:06 +0900

    refactor: (#101) 관리자 코드 삭제

--- simtong-presentation/src/main/kotlin/team/comit/simtong/admin/WebAdminAdapter.kt
+++ simtong-presentation/src/main/kotlin/team/comit/simtong/admin/WebAdminAdapter.kt
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import team.comit.simtong.admin.dto.request.WebAdminSignInRequest
 import team.comit.simtong.domain.auth.dto.TokenResponse
 import team.comit.simtong.domain.user.dto.SignInRequest
 import team.comit.simtong.domain.user.usecase.AdminSignInUseCase
+import team.comit.simtong.user.dto.request.WebSignInRequest
 import javax.validation.Valid
 
 /**
@@ -28,7 +28,7 @@ class WebAdminAdapter(
 
     @PostMapping("/tokens")
     @ResponseStatus(HttpStatus.OK)
-    fun signIn(@Valid @RequestBody request: WebAdminSignInRequest): TokenResponse {
+    fun signIn(@Valid @RequestBody request: WebSignInRequest): TokenResponse {
         return adminSignInUseCase.execute(
             SignInRequest(
                 employeeNumber = request.employeeNumber,
