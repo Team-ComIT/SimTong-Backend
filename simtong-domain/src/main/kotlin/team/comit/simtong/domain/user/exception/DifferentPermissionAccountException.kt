6f39b0e2a6e2b20a876f99e8eb7b63781e5bac52
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-12 19:17:33 +0900

    docs: (#101) 누락된 javadoc 추가

--- simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/exception/DifferentPermissionAccountException.kt
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/exception/DifferentPermissionAccountException.kt
@@ -3,6 +3,14 @@ package team.comit.simtong.domain.user.exception
 import team.comit.simtong.domain.user.error.UserErrorCode
 import team.comit.simtong.global.error.BusinessException
 
+/**
+ *
+ * 다른 권한의 계정 에러를 발생시키는 DifferentPermissionAccountException
+ *
+ * @author Chokyunghyeon
+ * @date 2022/10/12
+ * @version 1.0.0
+ **/
 class DifferentPermissionAccountException private constructor() : BusinessException(UserErrorCode.DIFFERENT_PERMISSION_ACCOUNT) {
 
     companion object {
