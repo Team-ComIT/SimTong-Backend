491327b4caae8fc62a891a9e6b8e233ee882ed56
KimBeomJin <sdpthf@naver.com>
2022-10-15 20:57:13 +0900

    test: (#115) ChangeSpotUseCaseTests

---
+++ simtong-application/src/test/kotlin/team/comit/simtong/domain/user/usecase/ChangeSpotUseCaseTests.kt
@@ -0,0 +1,114 @@
+package team.comit.simtong.domain.user.usecase
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import team.comit.simtong.domain.spot.exception.SpotNotFoundException
+import team.comit.simtong.domain.user.exception.UserNotFoundException
+import team.comit.simtong.domain.user.model.Authority
+import team.comit.simtong.domain.user.model.User
+import team.comit.simtong.domain.user.spi.CommandUserPort
+import team.comit.simtong.domain.user.spi.QueryUserPort
+import team.comit.simtong.domain.user.spi.UserQuerySpotPort
+import team.comit.simtong.domain.user.spi.UserSecurityPort
+import java.util.*
+
+@ExtendWith(SpringExtension::class)
+class ChangeSpotUseCaseTests {
+
+    @MockBean
+    private lateinit var queryUserPort: QueryUserPort
+
+    @MockBean
+    private lateinit var commandUserPort: CommandUserPort
+
+    @MockBean
+    private lateinit var querySpotPort: UserQuerySpotPort
+
+    @MockBean
+    private lateinit var userSecurityPort: UserSecurityPort
+
+    private lateinit var changeSpotUseCase: ChangeSpotUseCase
+
+    private val userId = UUID.randomUUID()
+    private val spotId = UUID.randomUUID()
+
+    private val userStub: User by lazy {
+        User(
+            id = userId,
+            nickname = "test nickname",
+            name = "test name",
+            email = "test email",
+            password = "test password",
+            employeeNumber = 1234567890,
+            authority = Authority.ROLE_COMMON,
+            spotId = spotId,
+            teamId = UUID.randomUUID(),
+            profileImagePath = "test path"
+        )
+    }
+
+    @BeforeEach
+    fun setUp() {
+        changeSpotUseCase = ChangeSpotUseCase(queryUserPort, commandUserPort, querySpotPort, userSecurityPort)
+    }
+
+    @Test
+    fun `지점 변경 성공`() {
+        // given
+        given(userSecurityPort.getCurrentUserId())
+            .willReturn(userId)
+
+        given(queryUserPort.queryUserById(userId))
+            .willReturn(userStub)
+
+        given(querySpotPort.existsSpotById(spotId))
+            .willReturn(true)
+
+        // when & then
+        assertDoesNotThrow {
+            changeSpotUseCase.execute(spotId)
+        }
+    }
+
+
+
+    @Test
+    fun `유저를 찾을 수 없음`() {
+        // given
+        given(userSecurityPort.getCurrentUserId())
+            .willReturn(userId)
+
+        given(queryUserPort.queryUserById(userId))
+            .willReturn(null)
+
+        // when & then
+        assertThrows<UserNotFoundException> {
+            changeSpotUseCase.execute(spotId)
+        }
+    }
+
+    @Test
+    fun `지점을 찾을 수 없음`() {
+        // given
+        given(userSecurityPort.getCurrentUserId())
+            .willReturn(userId)
+
+        given(queryUserPort.queryUserById(userId))
+            .willReturn(userStub)
+
+        given(querySpotPort.existsSpotById(spotId))
+            .willReturn(false)
+
+        // when & then
+        assertThrows<SpotNotFoundException> {
+            changeSpotUseCase.execute(spotId)
+        }
+    }
+
+}
