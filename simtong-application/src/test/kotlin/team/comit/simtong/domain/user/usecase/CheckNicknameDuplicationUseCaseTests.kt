95d8f949dbae32e89df495273af79060233cd12c
KimBeomJin <sdpthf@naver.com>
2022-10-15 21:04:11 +0900

    test: (#116) CheckNicknameDuplicationUseCaseTests

---
+++ simtong-application/src/test/kotlin/team/comit/simtong/domain/user/usecase/CheckNicknameDuplicationUseCaseTests.kt
@@ -0,0 +1,54 @@
+package team.comit.simtong.domain.user.usecase
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import team.comit.simtong.domain.auth.exception.UsedEmailException
+import team.comit.simtong.domain.auth.exception.UsedNicknameException
+import team.comit.simtong.domain.user.spi.QueryUserPort
+
+@ExtendWith(SpringExtension::class)
+class CheckNicknameDuplicationUseCaseTests {
+
+    @MockBean
+    private lateinit var queryUserPort: QueryUserPort
+
+    private lateinit var checkNicknameDuplicationUseCase: CheckNicknameDuplicationUseCase
+
+    private val nickname = "test nickname"
+
+    @BeforeEach
+    fun setUp() {
+        checkNicknameDuplicationUseCase = CheckNicknameDuplicationUseCase(queryUserPort)
+    }
+
+    @Test
+    fun `닉네임이 중복 되지 않음`() {
+        // given
+        given(queryUserPort.existsUserByNickname(nickname))
+            .willReturn(false)
+
+        // when & then
+        assertDoesNotThrow {
+            checkNicknameDuplicationUseCase.execute(nickname)
+        }
+    }
+
+    @Test
+    fun `닉네임이 중복됨`() {
+        // given
+        given(queryUserPort.existsUserByNickname(nickname))
+            .willReturn(true)
+
+        // when & then
+        assertThrows<UsedNicknameException> {
+            checkNicknameDuplicationUseCase.execute(nickname)
+        }
+    }
+
+}
