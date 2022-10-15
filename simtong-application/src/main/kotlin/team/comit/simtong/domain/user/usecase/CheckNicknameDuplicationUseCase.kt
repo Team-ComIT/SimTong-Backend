26abe2c17d16c1c44fd62d92353b660b0bd9be12
KimBeomJin <sdpthf@naver.com>
2022-10-15 13:29:16 +0900

    add: (#116) CheckNicknameDuplicationUseCase

---
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/user/usecase/CheckNicknameDuplicationUseCase.kt
@@ -0,0 +1,26 @@
+package team.comit.simtong.domain.user.usecase
+
+import team.comit.simtong.domain.auth.exception.UsedNicknameException
+import team.comit.simtong.domain.user.spi.QueryUserPort
+import team.comit.simtong.global.annotation.ReadOnlyUseCase
+
+/**
+ *
+ * 닉네임 중복 여부를 확인하는 CheckNicknameDuplicationUseCase
+ *
+ * @author kimbeomjin
+ * @date 2022/10/15
+ * @version 1.0.0
+ **/
+@ReadOnlyUseCase
+class CheckNicknameDuplicationUseCase(
+    private val queryUserPort: QueryUserPort
+) {
+
+    fun execute(nickname: String) {
+        if (queryUserPort.existsUserByNickname(nickname)) {
+            throw UsedNicknameException.EXCEPTION
+        }
+    }
+
+}
