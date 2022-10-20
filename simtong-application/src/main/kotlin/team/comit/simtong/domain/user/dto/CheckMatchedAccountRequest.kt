9b33cd2a8fd68f1b447966290bea25160c84cf1e
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-20 23:17:08 +0900

    refactor: (#130) Request 요소 변경

--- simtong-application/src/main/kotlin/team/comit/simtong/domain/user/dto/CheckMatchedAccountRequest.kt
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/user/dto/CheckMatchedAccountRequest.kt
@@ -2,13 +2,13 @@ package team.comit.simtong.domain.user.dto
 
 /**
  *
- * 해당 이름과 이메일을 가진 계정 여부 확인을 요청하는 CheckMatchedAccountRequest
+ * 해당 사원번호와 이메일을 가진 계정 여부 확인을 요청하는 CheckMatchedAccountRequest
  *
  * @author Chokyunghyeon
  * @date 2022/10/15
  * @version 1.0.0
  **/
 data class CheckMatchedAccountRequest(
-    val name: String,
+    val employeeNumber: Int,
     val email: String
 )
