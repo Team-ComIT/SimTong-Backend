c3524c85cbb7edecf6eed34488937f18485c3f6e
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-28 18:34:39 +0900

    add: (#132) domain yaml properties

---
+++ simtong-domain/src/main/kotlin/team/comit/simtong/global/DomainPropertiesKey.kt
@@ -0,0 +1,16 @@
+package team.comit.simtong.global
+
+object DomainPropertiesKey {
+
+    // User
+    const val USER_DEFAULT_IMAGE = "USER_DEFAULT_IMAGE"
+
+    // AuthCode
+    const val AUTHCODE_EXPIRED = "AUTHCODE_EXPIRED"
+
+    // AuthCodeLimit
+    const val AUTHCODELIMIT_EXPIRED = "AUTHCODELIMIT_EXPIRED"
+    const val AUTHCODELIMIT_VERIFIED_EXPIRED = "AUTHCODELIMIT_VERIFIED_EXPIRED"
+    const val AUTHCODELIMIT_MAX_ATTEMPT_COUNT = "AUTHCODELIMIT_MAX_ATTEMPT_COUNT"
+
+}
