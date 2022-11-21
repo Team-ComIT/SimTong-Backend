1885d943d7dd71ed804fb8b882e666be9427b2a5
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-22 08:55:11 +0900

    feat: (#139) 일정 저장 쿼리 추가

--- simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/schedule/SchedulePersistenceAdapter.kt
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/schedule/SchedulePersistenceAdapter.kt
@@ -1,12 +1,13 @@
 package team.comit.simtong.persistence.schedule
 
 import org.springframework.stereotype.Component
+import team.comit.simtong.domain.schedule.model.Schedule
 import team.comit.simtong.domain.schedule.spi.SchedulePort
 import team.comit.simtong.persistence.schedule.mapper.ScheduleMapper
 
 /**
  *
- * 일정의 영속성을 관리하는 SchedulePersistenceAdapter
+ * Schedule의 영속성을 관리하는 SchedulePersistenceAdapter
  *
  * @author Chokyunghyeon
  * @date 2022/11/21
@@ -18,5 +19,10 @@ class SchedulePersistenceAdapter(
     private val scheduleMapper: ScheduleMapper
 ) : SchedulePort {
 
+    override fun save(schedule: Schedule) = scheduleMapper.toDomain(
+       scheduleJpaRepository.save(
+           scheduleMapper.toEntity(schedule)
+       )
+    )!!
 
 }
