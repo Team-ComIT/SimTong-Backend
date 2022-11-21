de0678e20b7ab7b81c7ff67264bdf7249d4074b6
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-21 10:41:36 +0900

    add: (#137) 일정 애그리거트

---
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/schedule/model/Scope.kt
@@ -0,0 +1,22 @@
+package team.comit.simtong.domain.schedule.model
+
+/**
+ *
+ * 일정의 범위를 구분하는 Scope
+ *
+ * @author Chokyunghyeon
+ * @date 2022/11/21
+ * @version 1.0.0
+ **/
+enum class Scope {
+
+    /**
+     * 개인 일정
+     */
+    INDIVIDUAL,
+
+    /**
+     * 전체 일정
+     */
+    ENTIRE
+}
