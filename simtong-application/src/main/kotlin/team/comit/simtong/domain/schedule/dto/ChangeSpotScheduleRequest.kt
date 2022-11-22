90042e64c094e001fe939bcbac26a0643a88ba27
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-22 19:52:05 +0900

    add: (#141) 지점 일정 변경 Request

---
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/schedule/dto/ChangeSpotScheduleRequest.kt
@@ -0,0 +1,22 @@
+package team.comit.simtong.domain.schedule.dto
+
+import java.time.LocalDate
+import java.util.UUID
+
+/**
+ *
+ * 지점 일정 변경 기능 요청을 전달하는 ChangeSpotScheduleRequest
+ *
+ * @author Chokyunghyeon
+ * @date 2022/11/22
+ * @version 1.0.0
+ **/
+data class ChangeSpotScheduleRequest(
+    val scheduleId: UUID,
+
+    val title: String,
+
+    val startAt: LocalDate,
+
+    val endAt: LocalDate
+)
---
+++ simtong-presentation/src/main/kotlin/team/comit/simtong/schedule/dto/request/ChangeSpotScheduleWebRequest.kt
@@ -0,0 +1,29 @@
+package team.comit.simtong.schedule.dto.request
+
+import org.hibernate.validator.constraints.Length
+import java.time.LocalDate
+import javax.validation.constraints.FutureOrPresent
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
+/**
+ *
+ * 지점 일정을 변경 요청하는 ChangeSpotScheduleWebRequest
+ *
+ * @author Chokyunghyeon
+ * @date 2022/11/22
+ * @version 1.0.0
+ **/
+data class ChangeSpotScheduleWebRequest(
+    @field:NotBlank
+    @field:Length(max = 20)
+    val title: String,
+
+    @field:NotNull
+    @field:FutureOrPresent
+    val startAt: LocalDate,
+
+    // TODO 시작일, 종료일 검증 Resolver 구현
+    @field:NotNull
+    val endAt: LocalDate
+)
