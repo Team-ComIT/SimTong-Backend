3326b0ec302b064b56f436ffbf803a2f019fedc3
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-22 19:53:20 +0900

    feat: (#141) Schedule Adapter 수정

--- simtong-presentation/src/main/kotlin/team/comit/simtong/schedule/WebScheduleAdapter.kt
+++ simtong-presentation/src/main/kotlin/team/comit/simtong/schedule/WebScheduleAdapter.kt
@@ -3,13 +3,17 @@ package team.comit.simtong.schedule
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import team.comit.simtong.domain.schedule.dto.AddSpotScheduleRequest
+import team.comit.simtong.domain.schedule.dto.ChangeSpotScheduleRequest
 import team.comit.simtong.domain.schedule.usecase.AddSpotScheduleUseCase
+import team.comit.simtong.domain.schedule.usecase.ChangeSpotScheduleUseCase
 import team.comit.simtong.schedule.dto.request.AddSpotScheduleWebRequest
+import team.comit.simtong.schedule.dto.request.ChangeSpotScheduleWebRequest
 import java.util.UUID
 import javax.validation.Valid
 
@@ -24,7 +28,8 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/schedules")
 class WebScheduleAdapter(
-    private val addSpotScheduleUseCase: AddSpotScheduleUseCase
+    private val addSpotScheduleUseCase: AddSpotScheduleUseCase,
+    private val changeSpotScheduleUseCase: ChangeSpotScheduleUseCase
 ) {
 
     @PostMapping("/spots/{spot-id}")
@@ -34,6 +39,20 @@ class WebScheduleAdapter(
         addSpotScheduleUseCase.execute(
             AddSpotScheduleRequest(
                 spotId = spotId,
+                title = request.title,
+                startAt = request.startAt,
+                endAt = request.endAt
+            )
+        )
+    }
+
+    @PutMapping("/spots/{schedule-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun changeSpotSchedule(@PathVariable("schedule-id") scheduleId: UUID,
+                           @Valid @RequestBody request: ChangeSpotScheduleWebRequest) {
+        changeSpotScheduleUseCase.execute(
+            ChangeSpotScheduleRequest(
+                scheduleId = scheduleId,
                 title = request.title,
                 startAt = request.startAt,
                 endAt = request.endAt
