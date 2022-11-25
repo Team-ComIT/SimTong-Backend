73912a7913cd64dc95cbb4a771bb372e5858f00d
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-26 03:07:46 +0900

    docs: (#150) DateFormatConfig javadoc 추가

--- simtong-presentation/src/main/kotlin/team/comit/simtong/global/config/DateFormatConfig.kt
+++ simtong-presentation/src/main/kotlin/team/comit/simtong/global/config/DateFormatConfig.kt
@@ -5,6 +5,14 @@ import org.springframework.format.FormatterRegistry
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+/**
+ *
+ * LocalDateTime, LocalDate, LocalTime 요청값을 ISO-8601으로 설정하는 DateFormatConfig
+ *
+ * @author Chokyunghyeon
+ * @date 2022/11/26
+ * @version 1.0.0
+ **/
 @Configuration
 class DateFormatConfig : WebMvcConfigurer {
 
