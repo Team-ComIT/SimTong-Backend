4cbc3a5553013d7ce8b33c7fade58182c64676ed
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-12-01 22:43:55 +0900

    refactor: (#162) @Column 설정 변경

--- simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/menu/entity/MenuId.kt
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/menu/entity/MenuId.kt
@@ -6,12 +6,21 @@ import java.util.UUID
 import javax.persistence.Column
 import javax.persistence.Embeddable
 
+/**
+ *
+ * 메뉴 엔티티의 기본키인 MenuId
+ *
+ * @author Chokyunghyeon
+ * @date 2022/12/01
+ * @version 1.0.0
+ **/
 @Embeddable
 data class MenuId(
 
-    @Column(name = "spot_id", columnDefinition = "BINARY(16)")
+    @Column(nullable = false)
     val spotId: UUID,
 
+    @Column(nullable = false)
     val date: LocalDate
 
 ) : Serializable
--- simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/menu/entity/MenuJpaEntity.kt
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/menu/entity/MenuJpaEntity.kt
@@ -4,6 +4,7 @@ import team.comit.simtong.persistence.spot.entity.SpotJpaEntity
 import javax.persistence.EmbeddedId
 import javax.persistence.Entity
 import javax.persistence.FetchType
+import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 import javax.persistence.MapsId
 import javax.persistence.Table
@@ -27,8 +28,8 @@ class MenuJpaEntity(
     @field:NotNull
     val meal: String,
 
-    @field:NotNull
     @MapsId("spotId")
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "spot_id", columnDefinition = "BINARY(16)", nullable = false)
     val spot: SpotJpaEntity
 )
