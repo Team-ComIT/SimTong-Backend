a8e05a3a9977ab2fe8862efb171d5d7f980d9d2e
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-12-02 12:22:39 +0900

    add: (#164) 휴무일 Aggregate

---
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/holiday/model/Holiday.kt
@@ -0,0 +1,25 @@
+package team.comit.simtong.domain.holiday.model
+
+import team.comit.simtong.global.annotation.Aggregate
+import java.time.LocalDate
+import java.util.UUID
+
+/**
+ *
+ * 휴무일의 Root Aggregate를 담당하는 Holiday
+ *
+ * @author Chokyunghyeon
+ * @date 2022/12/02
+ * @version 1.0.0
+ **/
+@Aggregate
+data class Holiday(
+    val date: LocalDate,
+
+    val userId: UUID,
+
+    val type: HolidayType,
+
+    val spotId: UUID
+
+)
