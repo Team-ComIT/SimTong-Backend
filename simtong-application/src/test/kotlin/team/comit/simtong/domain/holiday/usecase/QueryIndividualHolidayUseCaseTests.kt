a711f74d9b1d22f2daf8bd31a9cbe782036831c7
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-12-05 22:05:18 +0900

    test: (#176) 개인 휴무일 조회 testcase

---
+++ simtong-application/src/test/kotlin/team/comit/simtong/domain/holiday/usecase/QueryIndividualHolidayUseCaseTests.kt
@@ -0,0 +1,79 @@
+package team.comit.simtong.domain.holiday.usecase
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.given
+import org.springframework.boot.test.mock.mockito.MockBean
+import team.comit.simtong.domain.holiday.dto.IndividualHolidayResponse
+import team.comit.simtong.domain.holiday.dto.QueryIndividualHolidaysResponse
+import team.comit.simtong.domain.holiday.model.Holiday
+import team.comit.simtong.domain.holiday.model.HolidayType
+import team.comit.simtong.domain.holiday.spi.HolidaySecurityPort
+import team.comit.simtong.domain.holiday.spi.QueryHolidayPort
+import team.comit.simtong.global.annotation.SimtongTest
+import java.time.LocalDate
+import java.util.UUID
+
+@SimtongTest
+class QueryIndividualHolidayUseCaseTests {
+
+    @MockBean
+    private lateinit var queryHolidayPort: QueryHolidayPort
+
+    @MockBean
+    private lateinit var securityPort: HolidaySecurityPort
+
+    private lateinit var queryIndividualHolidayUseCase: QueryIndividualHolidayUseCase
+
+    private val userId: UUID = UUID.randomUUID()
+
+    private val dateStub: LocalDate = LocalDate.now()
+
+    private val holidaysStub: List<Holiday> by lazy {
+        listOf(
+            Holiday(
+                date = LocalDate.now(),
+                userId = userId,
+                type = HolidayType.HOLIDAY,
+                spotId = UUID.randomUUID()
+            )
+        )
+    }
+
+    private val responseStub : QueryIndividualHolidaysResponse by lazy {
+        QueryIndividualHolidaysResponse(
+            holidaysStub.map {
+                IndividualHolidayResponse(
+                    date = it.date,
+                    type = it.type.name
+                )
+            }
+        )
+    }
+
+    @BeforeEach
+    fun setUp() {
+        queryIndividualHolidayUseCase = QueryIndividualHolidayUseCase(
+            queryHolidayPort = queryHolidayPort,
+            securityPort = securityPort
+        )
+    }
+
+    @Test
+    fun `개인 휴무일 조회 성공`() {
+        // given
+        given(securityPort.getCurrentUserId())
+            .willReturn(userId)
+
+        given(queryHolidayPort.queryHolidaysByMonthAndUserId(dateStub, userId))
+            .willReturn(holidaysStub)
+
+        // when
+        val response = queryIndividualHolidayUseCase.execute(dateStub)
+
+        // then
+        assertEquals(response, responseStub)
+    }
+
+}
