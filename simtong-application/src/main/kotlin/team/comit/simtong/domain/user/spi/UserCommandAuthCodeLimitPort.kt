ea665e54ea133ec37edd257c80622854c599e178
KimBeomJin <sdpthf@naver.com>
2022-12-06 15:22:19 +0900

    chore: (#184) 주석 추가

--- simtong-application/src/main/kotlin/team/comit/simtong/domain/user/spi/UserCommandAuthCodeLimitPort.kt
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/user/spi/UserCommandAuthCodeLimitPort.kt
@@ -2,6 +2,14 @@ package team.comit.simtong.domain.user.spi
 
 import team.comit.simtong.domain.auth.model.AuthCodeLimit
 
+/**
+ *
+ * User Domain에서 AuthCodeLimit Domain에 관한 Command를 요청하는UserCommandAuthCodeLimitPort
+ *
+ * @author kimbeomjin
+ * @date 2022/12/06
+ * @version 1.0.0
+ **/
 interface UserCommandAuthCodeLimitPort {
 
     fun delete(authCodeLimit: AuthCodeLimit)
