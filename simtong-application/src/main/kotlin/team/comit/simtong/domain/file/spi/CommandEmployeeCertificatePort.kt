63ccfbb486009483c790cd0f703c8e110e7db10c
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-12-06 15:11:31 +0900

    add: (#186) 직원 명부 Domain 관련 Port

---
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/file/spi/CommandEmployeeCertificatePort.kt
@@ -0,0 +1,13 @@
+package team.comit.simtong.domain.file.spi
+
+/**
+ *
+ * Employee Certificate Domain에 관한 명령을 요청하는 CommandEmployeeCertificatePort
+ *
+ * @author Chokyunghyeon
+ * @date 2022/12/06
+ * @version 1.0.0
+ **/
+interface CommandEmployeeCertificatePort {
+
+}
