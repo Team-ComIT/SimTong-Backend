9ba79e7bb1ece30fd656179e7e39f3d1171441a3
KimBeomJin <sdpthf@naver.com>
2022-12-07 22:30:57 +0900

    chore: (#191) javadoc 추가

--- simtong-domain/src/main/kotlin/team/comit/simtong/domain/file/exception/InvalidEmployeeException.kt
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/file/exception/InvalidEmployeeException.kt
@@ -3,6 +3,14 @@ package team.comit.simtong.domain.file.exception
 import team.comit.simtong.domain.file.error.FileErrorCode
 import team.comit.simtong.global.error.BusinessException
 
+/**
+ *
+ * DB에 저장된 일치하는 사원이 없는 경우 발생시키는 InvalidEmployeeException
+ *
+ * @author kimbeomjin
+ * @date 2022/12/07
+ * @version 1.0.0
+ **/
 object InvalidEmployeeException : BusinessException(
     FileErrorCode.INVALID_EMPLOYEE
 )
