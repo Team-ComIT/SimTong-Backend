ce8e0a0cd73b263c75803da2b0c22c180990abb6
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-12-07 22:31:51 +0900

    add: (#187) 사원 명부 등록 UseCase

---
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/file/usecase/RegisterEmployeeCertificateUseCase.kt
@@ -0,0 +1,28 @@
+package team.comit.simtong.domain.file.usecase
+
+import team.comit.simtong.domain.file.spi.CommandEmployeeCertificatePort
+import team.comit.simtong.domain.file.spi.ParseFilePort
+import team.comit.simtong.global.annotation.UseCase
+import java.io.File
+
+/**
+ *
+ * 직원 명부 등록 요청을 담당하는 RegisterEmployeeCertificateUseCase
+ *
+ * @author Chokyunghyeon
+ * @date 2022/12/06
+ * @version 1.0.0
+ **/
+@UseCase
+class RegisterEmployeeCertificateUseCase(
+    private val commandEmployeeCertificatePort: CommandEmployeeCertificatePort,
+    private val parseFilePort: ParseFilePort
+) {
+
+    fun execute(file: File) {
+        val employeeList = parseFilePort.importEmployeeCertificate(file)
+
+        commandEmployeeCertificatePort.saveAll(employeeList)
+    }
+
+}
