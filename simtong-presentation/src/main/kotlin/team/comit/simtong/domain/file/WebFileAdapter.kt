b7e93ea43c9fd973430d05b8d66926847c30a659
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-12-07 22:27:11 +0900

    feat: (#187) 사원 명부 저장 API 추가
    + File 변환 scope 변경

--- simtong-presentation/src/main/kotlin/team/comit/simtong/domain/file/WebFileAdapter.kt
+++ simtong-presentation/src/main/kotlin/team/comit/simtong/domain/file/WebFileAdapter.kt
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import team.comit.simtong.domain.file.usecase.UploadImageUseCase
 import team.comit.simtong.domain.file.dto.response.UploadImageListWebResponse
 import team.comit.simtong.domain.file.dto.response.UploadImageWebResponse
+import team.comit.simtong.domain.file.usecase.RegisterEmployeeCertificateUseCase
+import team.comit.simtong.domain.file.usecase.UploadImageUseCase
 import java.io.File
 import java.io.FileOutputStream
 import java.util.UUID
@@ -24,32 +25,38 @@ import java.util.UUID
 @RestController
 @RequestMapping("/files")
 class WebFileAdapter(
-    private val uploadImageUseCase: UploadImageUseCase
+    private val uploadImageUseCase: UploadImageUseCase,
+    private val registerEmployeeCertificateUseCase: RegisterEmployeeCertificateUseCase
 ) {
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun uploadSingleImage(file: MultipartFile): UploadImageWebResponse {
         return UploadImageWebResponse(
             uploadImageUseCase.execute(file.let(transferFile))
         )
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/list")
+    @ResponseStatus(HttpStatus.CREATED)
     fun uploadMultipleImage(files: List<MultipartFile>): UploadImageListWebResponse {
         return UploadImageListWebResponse(
             uploadImageUseCase.execute(files.map(transferFile))
         )
     }
 
+    @PostMapping("/employee")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun importEmployeeCertificate(file: MultipartFile) {
+        registerEmployeeCertificateUseCase.execute(file.let(transferFile))
+    }
+
     private val transferFile = { multipartFile: MultipartFile ->
-        File("${UUID.randomUUID()}@${multipartFile.originalFilename}").let {
-            FileOutputStream(it).run {
-                this.write(multipartFile.bytes)
-                this.close()
+        File("${UUID.randomUUID()}@${multipartFile.originalFilename}").apply {
+            FileOutputStream(this).run {
+                write(multipartFile.bytes)
+                close()
             }
-            it
         }
     }
 
