69804858d62c8bddc3c149574f6e566f9c891f02
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-12-19 12:02:29 +0900

    feat: (#223) 연차 지정 한도 초과 예외

--- simtong-domain/src/main/kotlin/team/comit/simtong/domain/holiday/exception/HolidayExceptions.kt
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/holiday/exception/HolidayExceptions.kt
@@ -20,9 +20,11 @@ sealed class HolidayExceptions(
 
     // 409
     class WeekHolidayLimitExcess(message: String = WEEK_HOLIDAY_LIMIT_EXCESS) : HolidayExceptions(409, message)
+    class AnnualLeaveLimitExcess(message: String = ANNUAL_LEAVE_LIMIT_EXCESS) : HolidayExceptions(409, message)
 
     companion object {
         private const val NOT_FOUND = "휴무일을 찾을 수 없습니다."
         private const val WEEK_HOLIDAY_LIMIT_EXCESS = "주 휴무일 지정 한도 초과"
+        private const val ANNUAL_LEAVE_LIMIT_EXCESS = "연차 지정 한도 초과"
     }
 }
