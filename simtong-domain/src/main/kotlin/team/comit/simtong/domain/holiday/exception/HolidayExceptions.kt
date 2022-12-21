0d6ecbf3895d5a1883d653af0d27749a52e0c93f
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-12-21 16:35:41 +0900

    feat: (#239) Already 예외 추가

--- simtong-domain/src/main/kotlin/team/comit/simtong/domain/holiday/exception/HolidayExceptions.kt
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/holiday/exception/HolidayExceptions.kt
@@ -18,12 +18,18 @@ sealed class HolidayExceptions(
     // 404
     class NotFound(message: String = NOT_FOUND) : HolidayExceptions(404, message)
 
+    // 409
+    class AlreadyHoliday(message: String = ALREADY_HOLIDAY) : HolidayExceptions(409, message)
+    class AlreadyAnnual(message: String = ALREADY_ANNUAL) : HolidayExceptions(409, message)
+
     // 429
     class WeekHolidayLimitExcess(message: String = WEEK_HOLIDAY_LIMIT_EXCESS) : HolidayExceptions(429, message)
     class AnnualLeaveLimitExcess(message: String = ANNUAL_LEAVE_LIMIT_EXCESS) : HolidayExceptions(429, message)
 
     companion object {
         private const val NOT_FOUND = "휴무일을 찾을 수 없습니다."
+        private const val ALREADY_HOLIDAY = "이미 휴무일입니다."
+        private const val ALREADY_ANNUAL = "이미 연차입니다."
         private const val WEEK_HOLIDAY_LIMIT_EXCESS = "주 휴무일 지정 한도 초과"
         private const val ANNUAL_LEAVE_LIMIT_EXCESS = "연차 지정 한도 초과"
     }
