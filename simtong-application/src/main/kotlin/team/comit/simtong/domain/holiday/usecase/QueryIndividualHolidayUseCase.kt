0ac85a3195f44ac825cbbb19208a5bf6c8aabf98
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-12-21 16:37:20 +0900

    refactor: (#239) 개인 휴무일 변경

--- simtong-application/src/main/kotlin/team/comit/simtong/domain/holiday/usecase/QueryIndividualHolidayUseCase.kt
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/holiday/usecase/QueryIndividualHolidayUseCase.kt
@@ -2,10 +2,11 @@ package team.comit.simtong.domain.holiday.usecase
 
 import team.comit.simtong.domain.holiday.dto.IndividualHolidayResponse
 import team.comit.simtong.domain.holiday.dto.QueryIndividualHolidaysResponse
+import team.comit.simtong.domain.holiday.dto.QueryIndividualRequest
+import team.comit.simtong.domain.holiday.model.HolidayStatus
 import team.comit.simtong.domain.holiday.spi.HolidaySecurityPort
 import team.comit.simtong.domain.holiday.spi.QueryHolidayPort
 import team.comit.simtong.global.annotation.ReadOnlyUseCase
-import java.time.LocalDate
 
 /**
  *
@@ -22,12 +23,16 @@ class QueryIndividualHolidayUseCase(
 ) {
 
     fun execute(
-        startAt: LocalDate,
-        endAt: LocalDate
+        request: QueryIndividualRequest
     ) : QueryIndividualHolidaysResponse {
         val currentUserId = securityPort.getCurrentUserId()
 
-        val holidays = queryHolidayPort.queryHolidaysByPeriodAndUserId(startAt, endAt, currentUserId)
+        val holidays = queryHolidayPort.queryHolidaysByPeriodAndUserIdAndStatus(
+            startAt = request.startAt,
+            endAt = request.endAt,
+            userId = currentUserId,
+            status = HolidayStatus.valueOf(request.status)
+        )
 
         val response = holidays.map {
             IndividualHolidayResponse(
