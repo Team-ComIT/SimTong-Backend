13554f01f8af4830a67c33c9bdba1d8734565641
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-12-21 23:39:25 +0900

    feat: (#245) 휴무일 유형에 따른 제약조건 추가

--- simtong-application/src/main/kotlin/team/comit/simtong/domain/holiday/usecase/CancelHolidayUseCase.kt
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/holiday/usecase/CancelHolidayUseCase.kt
@@ -1,6 +1,8 @@
 package team.comit.simtong.domain.holiday.usecase
 
 import team.comit.simtong.domain.holiday.exception.HolidayExceptions
+import team.comit.simtong.domain.holiday.model.HolidayStatus
+import team.comit.simtong.domain.holiday.model.HolidayType
 import team.comit.simtong.domain.holiday.spi.CommandHolidayPort
 import team.comit.simtong.domain.holiday.spi.HolidaySecurityPort
 import team.comit.simtong.domain.holiday.spi.QueryHolidayPort
@@ -28,6 +30,17 @@ class CancelHolidayUseCase(
         val holiday = queryHolidayPort.queryHolidayByDateAndUserId(date, currentUserId)
             ?: throw HolidayExceptions.NotFound()
 
+        when (holiday.type) {
+            HolidayType.HOLIDAY -> if (holiday.status == HolidayStatus.COMPLETED) {
+                throw HolidayExceptions.CannotChange()
+            }
+
+            HolidayType.ANNUAL -> if (holiday.date < LocalDate.now()) {
+                throw HolidayExceptions.CannotChange()
+            }
+        }
+
         commandHolidayPort.delete(holiday)
     }
+
 }
