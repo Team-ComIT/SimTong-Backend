193c31ae9c1823dafa8fa41c947736bfacf62215
KimBeomJin <sdpthf@naver.com>
2022-12-22 15:47:21 +0900

    add: (#248) EmployeeHolidayVO

---
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/holiday/vo/EmployeeHolidayVO.kt
@@ -0,0 +1,31 @@
+package team.comit.simtong.persistence.holiday.vo
+
+import com.querydsl.core.annotations.QueryProjection
+import team.comit.simtong.domain.holiday.model.HolidayType
+import team.comit.simtong.domain.holiday.spi.vo.EmployeeHoliday
+import java.time.LocalDate
+import java.util.UUID
+
+/**
+ *
+ * Holiday와 User 정보를 가져오는 EmployeeHolidayVO
+ *
+ * @author kimbeomjin
+ * @date 2022/12/22
+ * @version 1.0.0
+ **/
+class EmployeeHolidayVO @QueryProjection constructor(
+    date: LocalDate,
+    type: HolidayType,
+    userId: UUID,
+    userName: String,
+    teamName: String,
+    spotName: String
+) : EmployeeHoliday(
+    date = date,
+    type = type,
+    userId = userId,
+    userName = userName,
+    teamName = teamName,
+    spotName = spotName
+)
