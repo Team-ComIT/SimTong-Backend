06697ed052f9c834c256231c023ebad474ec4438
KimBeomJin <sdpthf@naver.com>
2022-12-30 17:58:44 +0900

    chore: (#261) 알림 전송시 type 추가

--- simtong-application/src/main/kotlin/team/comit/simtong/domain/notification/spi/SendPushMessagePort.kt
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/notification/spi/SendPushMessagePort.kt
@@ -1,5 +1,6 @@
 package team.comit.simtong.domain.notification.spi
 
+import team.comit.simtong.domain.notification.model.NotificationType
 import java.util.UUID
 
 /**
@@ -11,9 +12,8 @@ import java.util.UUID
  * @version 1.1.0
  **/
 interface SendPushMessagePort {
+    fun sendMessage(token: String, title: String, content: String, type: NotificationType, identify: UUID?)
 
-    fun sendMessage(tokens: List<String>, title: String, content: String, identify: UUID?)
-
-    fun sendMessage(token: String, title: String, content: String, identify: UUID?)
+    fun sendMessage(tokens: List<String>, title: String, content: String, type: NotificationType, identify: UUID?)
 
 }
--- simtong-infrastructure/src/main/kotlin/team/comit/simtong/thirdparty/notification/FcmAdapter.kt
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/thirdparty/notification/FcmAdapter.kt
@@ -7,6 +7,7 @@ import com.google.firebase.messaging.Message
 import com.google.firebase.messaging.MulticastMessage
 import com.google.firebase.messaging.Notification
 import org.springframework.stereotype.Component
+import team.comit.simtong.domain.notification.model.NotificationType
 import team.comit.simtong.domain.notification.spi.SendPushMessagePort
 import java.util.UUID
 
@@ -21,29 +22,10 @@ import java.util.UUID
 @Component
 class FcmAdapter : SendPushMessagePort {
 
-    override fun sendMessage(tokens: List<String>, title: String, content: String, identify: UUID?) {
-        val multicastMessage = MulticastMessage.builder()
-            .addAllTokens(tokens)
-            .putData("identify", identify.toString())
-            .setNotification(
-                Notification.builder()
-                    .setTitle(title)
-                    .setBody(content)
-                    .build()
-            )
-            .setApnsConfig(
-                ApnsConfig.builder()
-                    .setAps(Aps.builder().setSound("default").build())
-                    .build()
-            )
-            .build()
-
-        FirebaseMessaging.getInstance().sendMulticastAsync(multicastMessage)
-    }
-
-    override fun sendMessage(token: String, title: String, content: String, identify: UUID?) {
+    override fun sendMessage(token: String, title: String, content: String, type: NotificationType, identify: UUID?) {
         val message = Message.builder()
             .setToken(token)
+            .putData("type", type.name)
             .putData("identify", identify.toString())
             .setNotification(
                 Notification.builder()
@@ -59,5 +41,26 @@ class FcmAdapter : SendPushMessagePort {
             .build()
 
         FirebaseMessaging.getInstance().sendAsync(message)
+    }
+
+    override fun sendMessage(tokens: List<String>, title: String, content: String, type: NotificationType, identify: UUID?) {
+        val multicastMessage = MulticastMessage.builder()
+            .addAllTokens(tokens)
+            .putData("type", type.name)
+            .putData("identify", identify.toString())
+            .setNotification(
+                Notification.builder()
+                    .setTitle(title)
+                    .setBody(content)
+                    .build()
+            )
+            .setApnsConfig(
+                ApnsConfig.builder()
+                    .setAps(Aps.builder().setSound("default").build())
+                    .build()
+            )
+            .build()
+
+        FirebaseMessaging.getInstance().sendMulticastAsync(multicastMessage)
     }
 }
