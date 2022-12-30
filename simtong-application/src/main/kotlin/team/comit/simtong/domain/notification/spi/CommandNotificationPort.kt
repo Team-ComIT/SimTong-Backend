fb798e3d6359683174075bb9ef38949c003b9842
KimBeomJin <sdpthf@naver.com>
2022-12-30 18:20:21 +0900

    add: (#261) CommandNotificationPort

---
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/notification/spi/CommandNotificationPort.kt
@@ -0,0 +1,22 @@
+package team.comit.simtong.domain.notification.spi
+
+import team.comit.simtong.domain.notification.model.Notification
+import team.comit.simtong.domain.notification.model.NotificationReceiver
+
+/**
+ *
+ * Notification Domain에 관한 명령을 하는 CommandNotificationPort
+ *
+ * @author kimbeomjin
+ * @date 2022/12/30
+ * @version 1.1.0
+ **/
+interface CommandNotificationPort {
+
+    fun saveNotification(notification: Notification): Notification
+
+    fun saveNotificationReceiver(notificationReceiver: NotificationReceiver): NotificationReceiver
+
+    fun saveAllNotificationReceiver(notificationReceivers: List<NotificationReceiver>)
+
+}
