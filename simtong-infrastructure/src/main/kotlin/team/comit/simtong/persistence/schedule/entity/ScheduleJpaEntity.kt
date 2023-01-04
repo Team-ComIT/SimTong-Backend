8776e39f9bc4400db012e6f0b28931b22e6e88a1
KimBeomJin <sdpthf@naver.com>
2023-01-04 14:00:11 +0900

    chore: (#275) columnList 소문자 -> 대문자

--- simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/schedule/entity/ScheduleJpaEntity.kt
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/schedule/entity/ScheduleJpaEntity.kt
@@ -31,7 +31,7 @@ import javax.validation.constraints.NotNull
 @Table(
     name = "tbl_schedule",
     indexes = [
-        Index(name = "idx_start_at_end_at", columnList = "start_at ASC, end_at ASC")
+        Index(name = "idx_start_at_end_at", columnList = "startAt ASC, endAt ASC")
     ]
 )
 class ScheduleJpaEntity(
