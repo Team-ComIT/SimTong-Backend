26292d304f3ed387d89e1d2ce6217550f3eb7d3f
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-01-13 23:38:45 +0900

    fix: (#281) 정규식 오타 수정

--- simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/model/Password.kt
+++ simtong-domain/src/main/kotlin/team/comit/simtong/domain/user/model/Password.kt
@@ -22,7 +22,7 @@ value class Password(
          *
          * non-space, $ , + , - , _ , a ~ z , A ~ Z , 0 ~ 9
          */
-        const val PATTERN = """(?=.*[a-zA-Z])(?=.*\d)(?=^[\w$+-]{$MIN_LENGTH,$MAX_LENGTH$).*"""
+        const val PATTERN = """(?=.*[a-zA-Z])(?=.*\d)(?=^[\w$+-]{$MIN_LENGTH,$MAX_LENGTH}$).*"""
 
         fun of(password: String) = Password(password)
     }
