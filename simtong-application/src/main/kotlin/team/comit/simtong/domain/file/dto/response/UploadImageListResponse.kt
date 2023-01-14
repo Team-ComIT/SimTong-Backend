b053e142a632d01c78509f877daafef3cdbab7a4
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-01-14 23:28:21 +0900

    refactor: (#281) response 패키지 이동

--- simtong-application/src/main/kotlin/team/comit/simtong/domain/file/dto/response/UploadImageListResponse.kt
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/file/dto/response/UploadImageListResponse.kt
@@ -2,12 +2,12 @@ package team.comit.simtong.domain.file.dto.response
 
 /**
  *
- * 다중 이미지 업로드 요청 결과를 전송하는 UploadImageListWebResponse
+ * 다중 이미지 업로드 요청 결과를 전송하는 UploadImageListResponse
  *
  * @author Chokyunghyeon
  * @date 2022/09/21
- * @version 1.0.0
+ * @version 1.2.5
  **/
-data class UploadImageListWebResponse(
+data class UploadImageListResponse(
     val filePathList: List<String>
 )
