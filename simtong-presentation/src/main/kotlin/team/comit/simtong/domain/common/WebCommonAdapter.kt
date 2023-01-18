139d98f02bb1521d18bd5cde018a23b13444ca16
Jo KyungHyeon <khcho0125@icloud.com>
2023-01-19 00:56:23 +0900

    refactor: (#281) Value Class 제거 & dto 변경 적용

--- simtong-presentation/src/main/kotlin/team/comit/simtong/domain/common/WebCommonAdapter.kt
+++ simtong-presentation/src/main/kotlin/team/comit/simtong/domain/common/WebCommonAdapter.kt
@@ -4,22 +4,17 @@ import org.hibernate.validator.constraints.Range
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
-import team.comit.simtong.domain.auth.dto.TokenResponse
+import team.comit.simtong.domain.auth.dto.response.TokenResponse
 import team.comit.simtong.domain.auth.usecase.ReissueTokenUseCase
-import team.comit.simtong.domain.common.dto.request.ChangePasswordWebRequest
-import team.comit.simtong.domain.common.dto.request.FindEmployeeNumberWebRequest
-import team.comit.simtong.domain.common.dto.request.ResetPasswordWebRequest
-import team.comit.simtong.domain.common.dto.response.FindEmployeeNumberWebResponse
+import team.comit.simtong.domain.common.dto.ChangePasswordRequest
+import team.comit.simtong.domain.common.dto.FindEmployeeNumberRequest
+import team.comit.simtong.domain.common.dto.ResetPasswordRequest
 import team.comit.simtong.domain.spot.dto.SpotResponse
 import team.comit.simtong.domain.spot.usecase.ShowSpotListUseCase
 import team.comit.simtong.domain.team.dto.QueryTeamsResponse
 import team.comit.simtong.domain.team.usecase.QueryTeamsUseCase
-import team.comit.simtong.domain.user.dto.request.ChangePasswordData
-import team.comit.simtong.domain.user.dto.request.CheckMatchedAccountData
-import team.comit.simtong.domain.user.dto.request.FindEmployeeNumberData
-import team.comit.simtong.domain.user.dto.request.ResetPasswordData
+import team.comit.simtong.domain.user.dto.response.FindEmployeeNumberResponse
 import team.comit.simtong.domain.user.model.EmployeeNumber
-import team.comit.simtong.domain.user.model.Password
 import team.comit.simtong.domain.user.usecase.ChangePasswordUseCase
 import team.comit.simtong.domain.user.usecase.CheckEmailDuplicationUseCase
 import team.comit.simtong.domain.user.usecase.CheckMatchedAccountUseCase
@@ -37,7 +32,7 @@ import javax.validation.constraints.NotBlank
  *
  * @author Chokyunghyeon
  * @date 2022/09/11
- * @version 1.2.4
+ * @version 1.2.5
  **/
 @Validated
 @RestController
@@ -55,16 +50,8 @@ class WebCommonAdapter(
 ) {
 
     @GetMapping("/employee-number")
-    fun findEmployeeNumber(@Valid @ModelAttribute request: FindEmployeeNumberWebRequest): FindEmployeeNumberWebResponse {
-        val result = findEmployeeNumberUseCase.execute(
-            FindEmployeeNumberData(
-                name = request.name,
-                spotId = request.spotId,
-                email = request.email
-            )
-        )
-
-        return FindEmployeeNumberWebResponse(result)
+    fun findEmployeeNumber(@Valid @ModelAttribute request: FindEmployeeNumberRequest): FindEmployeeNumberResponse {
+        return findEmployeeNumberUseCase.execute(request.toData())
     }
 
     @PutMapping("/token/reissue")
@@ -74,14 +61,8 @@ class WebCommonAdapter(
 
     @PutMapping("/password/initialization")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun resetPassword(@Valid @RequestBody request: ResetPasswordWebRequest) {
-        resetPasswordUseCase.execute(
-            ResetPasswordData(
-                email = request.email,
-                employeeNumber = request.employeeNumber.value,
-                newPassword = request.newPassword.value
-            )
-        )
+    fun resetPassword(@Valid @RequestBody request: ResetPasswordRequest) {
+        resetPasswordUseCase.execute(request.toData())
     }
 
     @GetMapping("/email/duplication")
@@ -91,28 +72,18 @@ class WebCommonAdapter(
 
     @PutMapping("/password")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun changePassword(@Valid @RequestBody request: ChangePasswordWebRequest) {
-        changePasswordUseCase.execute(
-            ChangePasswordData(
-                password = request.password.value,
-                newPassword = request.newPassword.value
-            )
-        )
+    fun changePassword(@Valid @RequestBody request: ChangePasswordRequest) {
+        changePasswordUseCase.execute(request.toData())
     }
 
     @GetMapping("/account/existence")
     fun checkMatchedAccount(
         @Range(min = EmployeeNumber.MIN_VALUE, max = EmployeeNumber.MAX_VALUE)
-        @RequestParam employeeNumber: EmployeeNumber,
+        @RequestParam employeeNumber: Int,
         @NotBlank @Email
         @RequestParam email: String
     ) {
-        checkMatchedAccountUseCase.execute(
-            CheckMatchedAccountData(
-                employeeNumber = employeeNumber.value,
-                email = email
-            )
-        )
+        checkMatchedAccountUseCase.execute(employeeNumber, email)
     }
 
     @GetMapping("/spot")
@@ -121,8 +92,8 @@ class WebCommonAdapter(
     }
 
     @GetMapping("/password/compare")
-    fun comparePassword(@NotBlank @RequestParam password: Password) {
-        comparePasswordUseCase.execute(password.value)
+    fun comparePassword(@NotBlank @RequestParam password: String) {
+        comparePasswordUseCase.execute(password)
     }
 
     @GetMapping("/team/{spot-id}")
