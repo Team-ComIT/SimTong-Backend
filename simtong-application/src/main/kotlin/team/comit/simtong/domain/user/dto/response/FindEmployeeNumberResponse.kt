de3cf318c6f0f21a4ca0b6793edfe9a895f7c2f6
Jo KyungHyeon <khcho0125@icloud.com>
2023-01-19 00:53:57 +0900

    refactor: (#281) WebResponse → Response 변경

---
+++ simtong-application/src/main/kotlin/team/comit/simtong/domain/user/dto/response/FindEmployeeNumberResponse.kt
@@ -0,0 +1,13 @@
+package team.comit.simtong.domain.user.dto.response
+
+/**
+ *
+ * 사원 번호 찾기 결과를 전송하는 FindEmployeeNumberResponse
+ *
+ * @author Chokyunghyeon
+ * @date 2022/09/11
+ * @version 1.2.5
+ **/
+data class FindEmployeeNumberResponse(
+    val employeeNumber: Int
+)
